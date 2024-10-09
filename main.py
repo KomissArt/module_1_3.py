a79ac92931072064b53832804f9295d837f879e1
KomissArt <komisst6@gmail.com>
2024-10-10 00:20:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from types import new_class
+name='Татьяна'
+print('Name:', name)
+age=35
+print('Age:', age)
+new_age=age+1
+print('New Age:', new_age)
+is_student = True
+print('Is steden:', is_student)
